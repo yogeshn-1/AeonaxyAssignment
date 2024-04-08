7c1850897bd408cf54e11b119421766f2cdbd8b0
yogeshn-1 <nirmalkary@gmail.com>
2024-04-08 11:14:20 +0530

    Updating header layout

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,9 +3,9 @@ import logo from "../img/logo.svg";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <nav className="w-full">
-      <div className="p-3 flex items-center justify-between mx-auto max-w-7xl relative">
-        <img alt="Calendly logo" className="h-8" src={logo} />
+    <nav className="w-full sticky top-0 bg-white shadow-xl">
+      <div className="px-6 py-4 flex items-center justify-between ">
+        <img alt="Calendly logo" className="h-8 " src={logo} />
         <div className="flex gap-4 md:hidden xs:hidden sm:hidden">
           <a href="#" className="hover:text-blue-400 ">
             Individuals
@@ -30,14 +30,14 @@ const Header = () => {
           <a href="#" className="hover:text-blue-400">
             Login
           </a>
-          <button className="py-3 px-6 mx-2 bg-blue-500 hover:bg-blue-700 text-white rounded-full w-fit duration-200">
+          <button className="py-2 px-5 mx-2 bg-blue-500 hover:bg-blue-700 text-white rounded-full w-fit duration-200">
             Get Started
           </button>
         </div>
-        <div className="block lg:hidden xl:hidden 2xl:hidden">
+        <div className="block lg:hidden xl:hidden ">
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="flex items-center px-3 py-2 border rounded text-black border-white hover:text-black hover:border-white "
+            className="flex items-center px-3 py-2 border rounded text-black border-gray-700  hover:border-black absolute top-5 right-5 z-30"
           >
             <svg
               className="fill-current h-3 w-3"
@@ -48,25 +48,38 @@ const Header = () => {
             </svg>
           </button>
         </div>
-        <div
-          className={`${
-            isMenuOpen ? "hidden" : "flex"
-          } right-0 top-10 z-10 w-1/3  p-4 h-screen text-sm bg-white/80 rounded xl flex-col absolute lg:hidden xl:hidden 2xl:hidden`}
-        >
-          <span className="hover:text-blue-400 border-b mb-1">Individuals</span>
-          <span className="hover:text-blue-400 border-b mb-1">Teams</span>
-          <span className="hover:text-blue-400 border-b mb-1">Enterprises</span>
-          <span className="hover:text-blue-400 border-b mb-1">Product</span>
-          <span className="hover:text-blue-400 border-b mb-1">Pricing</span>
-          <span className="hover:text-blue-400 border-b mb-1">Resource</span>
-          <div className="flex gap-2 m-2 xs:flex-col">
-            <a href="#" className="hover:text-blue-400">
-              Login
-            </a>
-            <button className="px-2 py-1 bg-blue-500 rounded-2xl w-fit text-nowrap">
-              Get Started
-            </button>
-          </div>
+      </div>
+      <div
+        className={`${
+          isMenuOpen ? "hidden" : "flex"
+        } w-full p-4 min-h-fit text-sm bg-white rounded-xl flex-col shadow-xl absolute right-0 top-0 z-10 lg:hidden xl:hidden `}
+      >
+        <img alt="Calendly logo" className="h-8 self-start" src={logo} />
+        <span className="hover:text-blue-400 mb-2 w-full text-center">
+          Individuals
+        </span>
+        <span className="hover:text-blue-400 mb-2 w-full text-center">
+          Teams
+        </span>
+        <span className="hover:text-blue-400 mb-2 w-full text-center">
+          Enterprises
+        </span>
+        <span className="hover:text-blue-400 mb-2 w-full text-center">
+          Product
+        </span>
+        <span className="hover:text-blue-400 mb-2 w-full text-center">
+          Pricing
+        </span>
+        <span className="hover:text-blue-400 mb-2 w-full text-center">
+          Resource
+        </span>
+        <div className="flex gap-2 m-2 xs:flex-col xs:items-center w-full justify-center">
+          <a href="#" className="hover:text-blue-400">
+            Login
+          </a>
+          <button className="px-2 py-1 bg-blue-500 rounded-2xl text-white w-fit text-nowrap">
+            Get Started
+          </button>
         </div>
       </div>
     </nav>
